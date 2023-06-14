a1937e3bc1d9bf66acdc71f5e76776a08209eea7
hasanrana21 <hasanrana178821@gmail.com>
2023-06-14 19:59:47 +0600

    feat: axios added and list api integrations

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
-
+import axios from "axios";
+window.axios = axios;
 createApp(App).mount("#app");
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,26 +1,10 @@
 <template>
   <div>
     <ui-table>
-      <ui-table-tr>
-        <ui-table-td>Hasan</ui-table-td>
-        <ui-table-td>4.00</ui-table-td>
-        <ui-table-td>1200tk</ui-table-td>
-        <ui-table-td>
-          <ui-button label="Show" />
-        </ui-table-td>
-      </ui-table-tr>
-      <ui-table-tr>
-        <ui-table-td>Hasan</ui-table-td>
-        <ui-table-td>4.00</ui-table-td>
-        <ui-table-td>1200tk</ui-table-td>
-        <ui-table-td>
-          <ui-button label="Show" />
-        </ui-table-td>
-      </ui-table-tr>
-      <ui-table-tr>
-        <ui-table-td>Hasan</ui-table-td>
-        <ui-table-td>4.00</ui-table-td>
-        <ui-table-td>1200tk</ui-table-td>
+      <ui-table-tr v-for="(product, key) in state.products" :key="key">
+        <ui-table-td>{{ product?.title }}</ui-table-td>
+        <ui-table-td>{{ product?.rating }}</ui-table-td>
+        <ui-table-td>{{ product?.price }}</ui-table-td>
         <ui-table-td>
           <ui-button label="Show" />
         </ui-table-td>
@@ -33,13 +17,19 @@ import UiTable from "../../components/ui/table/index.vue";
 import UiTableTr from "../../components/ui/table/tr.vue";
 import UiTableTd from "../../components/ui/table/td.vue";
 import UiButton from "../../components/ui/button/index.vue";
-import { reactive } from "vue";
+import { onMounted, reactive } from "vue";
 export default {
   name: "home-page",
   components: { UiTable, UiTableTr, UiTableTd, UiButton },
   setup() {
     const state = reactive({
       products: [],
+    });
+    onMounted(() => {
+      axios.get("https://dummyjson.com/products").then((res) => {
+        console.log("data", res.data.products);
+        state.products = res.data?.products;
+      });
     });
     return {
       state,
